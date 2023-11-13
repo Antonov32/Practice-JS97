a5956204213b349aef84c6462f8cfc7c49836dce
Antonov32 <112275153+Antonov32@users.noreply.github.com>
2023-11-13 22:17:47 +0100

    practice JS object values

--- my-practice.js
+++ my-practice.js
@@ -564,37 +564,72 @@
 
 // Напиши функцію getAllPropValues(propName), яка приймає один параметр propName - ім'я (ключ) властивості. Функція повинна повернути масив усіх значень властивості з таким ім'ям з кожного об'єкта в масиві products. Якщо в об'єктах відсутні властивості з таким ім'ям, функція повинна повернути порожній масив.
 
-function getAllPropValues(propName) {
-  const products = [
-    { name: "Radar", price: 1300, quantity: 4 },
-    { name: "Scanner", price: 2700, quantity: 3 },
-    { name: "Droid", price: 400, quantity: 7 },
-    { name: "Grip", price: 1200, quantity: 9 },
-  ];
+// function getAllPropValues(propName) {
+//   const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
 
-  const nameArray = [];
-  const priceArray = [];
-  const quantityArray = [];
-  const nameToSearchFor = propName;
+//   const nameArray = [];
+//   const priceArray = [];
+//   const quantityArray = [];
+//   const nameToSearchFor = propName;
 
-  for (const product of products) {
-    nameArray.push(product.name);
-    priceArray.push(product.price);
-    quantityArray.push(product.quantity);
-  }
+//   for (const product of products) {
+//     nameArray.push(product.name);
+//     priceArray.push(product.price);
+//     quantityArray.push(product.quantity);
+//   }
 
-  if (nameToSearchFor === "name") {
-    return nameArray;
-  } else if (nameToSearchFor === "price") {
-    return priceArray;
-  } else if (nameToSearchFor === "quantity") {
-    return quantityArray;
-  } else {
-    return [];
-  }
-}
+//   if (nameToSearchFor === "name") {
+//     return nameArray;
+//   } else if (nameToSearchFor === "price") {
+//     return priceArray;
+//   } else if (nameToSearchFor === "quantity") {
+//     return quantityArray;
+//   } else {
+//     return [];
+//   }
+// }
 
-console.log(getAllPropValues("name")); // повертає ["Radar", "Scanner", "Droid", "Grip"]
-console.log(getAllPropValues("quantity")); // повертає [4, 3, 7, 9]
-console.log(getAllPropValues("price")); // повертає [1300, 2700, 400, 1200]
-console.log(getAllPropValues("category")); // повертає []
+// console.log(getAllPropValues("name")); // повертає ["Radar", "Scanner", "Droid", "Grip"]
+// console.log(getAllPropValues("quantity")); // повертає [4, 3, 7, 9]
+// console.log(getAllPropValues("price")); // повертає [1300, 2700, 400, 1200]
+// console.log(getAllPropValues("category")); // повертає []
+
+//*
+//
+// ------------------------------------------------------------
+//
+//*
+
+// Доповни код функції так, щоб вона повертала загальну вартість (ціна * кількість) товару з таким ім'ям з масиву products.
+
+// Якщо продукту з такою назвою немає, то функція повинна повертати рядок "Product <productName> not found!" , де <productName> — це ім'я товару.
+
+// function calculateTotalPrice(productName) {
+//   const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
+
+//   const nameToSearchFor = productName;
+
+//   for (const product of products) {
+//     if (nameToSearchFor === product.name) {
+//       return product.price * product.quantity;
+//     }
+//   }
+//   return `Product ${nameToSearchFor} not found!`;
+// }
+
+// Оголошена функція calculateTotalPrice(productName)
+// console.log(calculateTotalPrice("Blaster")); // повертає "Product Blaster not found!"
+// console.log(calculateTotalPrice("Radar")); // повертає 5200
+// console.log(calculateTotalPrice("Droid")); // повертає 2800
+// console.log(calculateTotalPrice("Grip")); // повертає 10800
+// console.log(calculateTotalPrice("Scanner")); // повертає 8100
