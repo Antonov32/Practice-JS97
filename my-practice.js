0484ad62f215a104f8a36e40e6abe1302ef87778
Antonov32 <112275153+Antonov32@users.noreply.github.com>
2023-11-20 18:25:32 +0100

    practice JS callback method forEach

--- my-practice.js
+++ my-practice.js
@@ -865,3 +865,161 @@
 // ------------------------------------------------------------
 //
 //*
+
+// Функція makeMessage приймає один параметр ім'я піци, що доставляється, pizzaName та повертає рядок з повідомленням клієнту.
+
+// Доповни функцію makeMessage таким чином, щоб вона очікувала другим параметром (параметр callback) колбек-функцію і повертала результат її виклику. Функції deliverPizza або makePizza будуть передаватися як колбек для makeMessage і очікувати аргументом ім'я готової піци, що доставляється.
+
+// function deliverPizza(pizzaName) {
+//   return `Delivering ${pizzaName} pizza.`;
+// }
+
+// function makePizza(pizzaName) {
+//   return `Pizza ${pizzaName} is being prepared, please wait...`;
+// }
+
+// function makeMessage(pizzaName, callback) {
+//   return callback(pizzaName);
+// }
+
+// console.log(makeMessage("Royal Grand", makePizza)); // "Pizza Royal Grand is being prepared, please wait..."
+
+// console.log(makeMessage("Ultracheese", deliverPizza)); // повертає рядок "Delivering Ultracheese pizza."
+
+//*
+//
+// ------------------------------------------------------------
+//
+//*
+
+// Функція makePizza має два параметри: pizzaName - ім'я піци та callback - колбек-функцію. Під час виконання, makePizza викликає цей колбек, передаючи йому pizzaName як аргумент.
+
+// Доповни другий виклик функції makePizza(pizzaName, callback), передавши другим аргументом інлайн колбек-функцію eatPizza(pizzaName). Колбек eatPizza логує рядок "Eating pizza <назва піци>", де <назва піци> це значення параметра pizzaName.
+
+// function makePizza(pizzaName, callback) {
+//   console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
+//   callback(pizzaName);
+// }
+
+// makePizza("Royal Grand", function deliverPizza(pizzaName) {
+//   console.log(`Delivering pizza ${pizzaName}`);
+// });
+
+// makePizza("Ultracheese", function eatPizza(pizzaName) {
+//   console.log(`Eating pizza ${pizzaName}`);
+// });
+
+//*
+//
+// ------------------------------------------------------------
+//
+//*
+
+// Функція calculateTotalPrice(orderedItems) приймає один параметр orderedItems - масив чисел, і розраховує загальну суму його елементів, яка зберігається у змінній totalPrice і повертається як результат роботи функції.
+
+// Доповни виклик метода forEach, передавши йому колбек-функцію, яка на кожній ітерації додає до totalPrice значення поточного елемента масива orderedItems.
+
+// function calculateTotalPrice(orderedItems) {
+//   let totalPrice = 0;
+
+//   orderedItems.forEach(function (item) {
+//     totalPrice += item;
+//   });
+//   return totalPrice;
+// }
+
+// console.log(calculateTotalPrice([12, 85, 37, 4])); // повертає 138
+// console.log(calculateTotalPrice([164, 48, 291])); // повертає 503
+// console.log(calculateTotalPrice([412, 371, 94, 63, 176])); // повертає 1116
+
+//*
+//
+// ------------------------------------------------------------
+//
+//*
+
+// Функція filterArray(numbers, value) приймає першим параметром масив чисел numbers і повертає новий масив, в якому будуть тільки ті елементи оригінального масиву, які більші за значення другого параметра числа value. Якщо таких значень не буде знайдено, функція повертає порожній масив.
+
+// Виконай рефакторинг функції таким чином, щоб замість циклу for, вона використовувала метод forEach.
+
+// function filterArray(numbers, value) {
+//   const newArray = [];
+
+//   numbers.forEach((numbers) => {
+//     if (numbers > value) {
+//       newArray.push(numbers);
+//     }
+//   });
+//   return newArray;
+// }
+
+// console.log(filterArray([1, 2, 3, 4, 5], 3)); //  повертає[4, 5]
+// console.log(filterArray([1, 2, 3, 4, 5], 4)); //  повертає[5]
+// console.log(filterArray([1, 2, 3, 4, 5], 5)); //  повертає[]
+// console.log(filterArray([12, 24, 8, 41, 76], 38)); //  повертає[41, 76]
+// console.log(filterArray([12, 24, 8, 41, 76], 20)); //  повертає [24, 41, 76]
+
+//*
+//
+// ------------------------------------------------------------
+//
+//*
+
+// Виконай рефакторинг функції calculateTotalPrice(orderedItems), замінивши її оголошення на стрілочну функцію. Також заміни колбек-функцію, передану в метод forEach(), на стрілочну функцію.
+
+// function calculateTotalPrice(orderedItems) {
+//   let totalPrice = 0;
+
+//   orderedItems.forEach(function (item) {
+//     totalPrice += item;
+//   });
+
+//   return totalPrice;
+// }
+
+// const calculateTotalPrice = (orderedItems) => {
+//   let totalPrice = 0;
+
+//   orderedItems.forEach((item) => (totalPrice += item));
+//   return totalPrice;
+// };
+
+// console.log(calculateTotalPrice([12, 85, 37, 4])); // повертає 138
+// console.log(calculateTotalPrice([164, 48, 291])); // повертає 503
+// console.log(calculateTotalPrice([412, 371, 94, 63, 176])); // повертає 1116
+
+//*
+//
+// ------------------------------------------------------------
+//
+//*
+
+// Заміни оголошення функції filterArray() і колбек для методу forEach() на стрілочні функції.
+
+// function filterArray(numbers, value) {
+//   const filteredNumbers = [];
+
+//   numbers.forEach(function (number) {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   });
+
+//   return filteredNumbers;
+// }
+
+// const filterArray = (numbers, value) => {
+//   const filteredNumbers = [];
+//   numbers.forEach((number) => {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   });
+//   return filteredNumbers;
+// };
+
+// console.log(filterArray([1, 2, 3, 4, 5], 3)); //  повертає[4, 5]
+// console.log(filterArray([1, 2, 3, 4, 5], 4)); //  повертає[5]
+// console.log(filterArray([1, 2, 3, 4, 5], 5)); //  повертає[]
+// console.log(filterArray([12, 24, 8, 41, 76], 38)); //  повертає[41, 76]
+// console.log(filterArray([12, 24, 8, 41, 76], 20)); //  повертає [24, 41, 76]
