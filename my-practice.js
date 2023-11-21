ee709a2fbadda4602a3e97df21760ddb90003d15
Antonov32 <112275153+Antonov32@users.noreply.github.com>
2023-11-21 20:25:28 +0100

    practice JS function map

--- my-practice.js
+++ my-practice.js
@@ -1023,3 +1023,43 @@
 // console.log(filterArray([1, 2, 3, 4, 5], 5)); //  повертає[]
 // console.log(filterArray([12, 24, 8, 41, 76], 38)); //  повертає[41, 76]
 // console.log(filterArray([12, 24, 8, 41, 76], 20)); //  повертає [24, 41, 76]
+
+//*
+//
+// ------------------------------------------------------------
+//
+//*
+
+// Функція changeEven(numbers, value) приймає масив чисел numbers і оновлює кожен елемент, значення якого - це парне число, додаючи до нього значення параметра value, який точно є числом.
+
+// Виконай рефакторинг функції таким чином, щоб вона стала чистою - не змінювала масив чисел numbers, а створювала, наповнювала і повертала новий масив з оновленими значеннями.
+
+// function changeEven(numbers, value) {
+//   for (let i = 0; i < numbers.length; i += 1) {
+//     if (numbers[i] % 2 === 0) {
+//       numbers[i] = numbers[i] + value;
+//     }
+//   }
+//   return numbers;
+// }
+
+// const changeEven = (numbers, value) => {
+//   let newNumbers = [];
+
+//   numbers.forEach((element) => {
+//     if (element % 2 === 0) {
+//       newNumbers.push(element + value);
+//     } else {
+//       newNumbers.push(element);
+//     }
+//   });
+// return newNumbers;
+// };
+
+// Оголошена функція changeEven(numbers, value)
+// Функція changeEven не змінює значення параметра numbers
+// console.log(changeEven([1, 2, 3, 4, 5], 10)); // повертає новий масив [1, 12, 3, 14, 5]
+// console.log(changeEven([2, 8, 3, 7, 4, 6], 10)); // повертає новий масив [12, 18, 3, 7, 14, 16]
+// console.log(changeEven([17, 24, 68, 31, 42], 100)); // повертає новий масив [17, 124, 168, 31, 142]
+// console.log(changeEven([44, 13, 81, 92, 36, 54], 100)); // повертає новий масив [144, 13, 81, 192, 136, 154]
+// Виклик функції з випадковими, але валідними аргументами, повертає правильне значення
