15fab7bc57d9afdb8f921d8228600f33789a11f0
Antonov32 <112275153+Antonov32@users.noreply.github.com>
2023-11-22 21:02:33 +0100

    practice JS method toSorted

--- my-practice.js
+++ my-practice.js
@@ -1098,3 +1098,74 @@
 // console.log(totalAveragePlaytimePerGame);
 // Значення змінної totalAveragePlaytimePerGame - це число 1023
 // Для перебирання масиву players використовується метод reduce()
+
+//*
+//
+// ------------------------------------------------------------
+//
+//*
+
+// Масив books містить масив об'єктів книг, кожен з яких містить властивості title, author, rating.
+
+// Доповни код таким чином, щоб:
+
+// У змінній sortedByAuthorName вийшов масив книг, відсортований за ім'ям автора в алфавітному порядку.
+// У змінній sortedByReversedAuthorName вийшов масив книг, відсортований за ім'ям автора у зворотному алфавітному порядку.
+// У змінній sortedByAscendingRating вийшов масив книг, відсортований за зростанням рейтингу.
+// У змінній sortedByDescentingRating вийшов масив книг, відсортований за спаданням рейтингу.
+
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//   },
+//   {
+//     title: "The Dream of a Ridiculous Man",
+//     author: "Fyodor Dostoevsky",
+//     rating: 7.75,
+//   },
+//   {
+//     title: "Redder Than Blood",
+//     author: "Tanith Lee",
+//     rating: 7.94,
+//   },
+//   {
+//     title: "Enemy of God",
+//     author: "Bernard Cornwell",
+//     rating: 8.67,
+//   },
+// ];
+
+// const sortedByAuthorName = books.toSorted((firstAuthor, secondAuthor) =>
+//   firstAuthor.author[0].localeCompare(secondAuthor.author[0])
+// );
+
+// const sortedByReversedAuthorName = books.toSorted((firstAuthor, secondAuthor) =>
+//   secondAuthor.author[0].localeCompare(firstAuthor.author[0])
+// );
+
+// const sortedByAscendingRating = books.toSorted(
+//   (firstAuthor, secondAuthor) => firstAuthor.rating - secondAuthor.rating
+// );
+
+// const sortedByDescentingRating = books.toSorted(
+//   (firstAuthor, secondAuthor) => secondAuthor.rating - firstAuthor.rating
+// );
+
+// Оголошена змінна books
+// Значення змінної books - це вихідний масив об'єктів книг
+// console.log(sortedByAuthorName);
+// Значення змінної sortedByAuthorName - це масив книг, відсортований за ім'ям автора в алфавітному порядку
+// console.log(sortedByReversedAuthorName);
+// Значення змінної sortedByReversedAuthorName - це масив книг, відсортований за ім'ям автора у зворотному алфавітному порядку
+// console.log(sortedByAscendingRating);
+// Значення змінної sortedByAscendingRating - це масив книг, відсортований за зростанням рейтингу
+// console.log(sortedByDescentingRating);
+// Значення змінної sortedByDescentingRating - це масив книг, відсортований за спаданням рейтингу
+// Для перебирання масиву books використовується метод toSorted()
