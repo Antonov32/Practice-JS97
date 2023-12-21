0c7499168eeaf6ad8663d797e386060706d0a00f
Antonov32 <112275153+Antonov32@users.noreply.github.com>
2023-12-21 13:06:03 +0100

    add examples innerHTML and insertAdjacentHTML

--- DOM/dom.js
+++ DOM/dom.js
@@ -183,3 +183,38 @@
 // ---------------------------<!-- Practice Modul 7 -->-----------------------
 //
 //*
+
+//*
+//
+// ---------------------------<!-- Властивість innerHTML -->-----------------------
+//
+//*
+
+// const technologies = ["HTML", "CSS", "JavaScript", "React", "Node"];
+// const list = document.querySelector(".list");
+
+// const markup = technologies
+//   .map((technology) => `<li class="list-item">${technology}</li>`)
+//   .join("");
+
+// // Check the console, you'll see a single string with HTML tags
+// console.log(markup);
+
+// // Adding all the markup in one operation
+// list.innerHTML = markup;
+
+//*
+//
+// ---------------------------<!-- Метод insertAdjacentHTML() -->-----------------------
+//
+//*
+
+const list = document.querySelector(".list");
+
+const newTechnologies = ["React", "TypeScript", "Node.js"];
+const markup = newTechnologies
+  .map((technology) => `<li class="list-item new">${technology}</li>`)
+  .join("");
+
+list.insertAdjacentHTML("beforeend", markup);
+list.insertAdjacentHTML("beforebegin", "<h2>Popular technologies</h2>");
