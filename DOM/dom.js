df06aef32110651c31eb1e68286b127da5d20c37
Antonov32 <112275153+Antonov32@users.noreply.github.com>
2023-12-26 12:03:21 +0100

    practice Delegation of events

--- DOM/dom.js
+++ DOM/dom.js
@@ -416,3 +416,113 @@
 // textInput.addEventListener("blur", () => {
 //   textInput.value = "";
 // });
+
+//*
+//
+// ---------------------------<!-- Делегування подій -->-----------------------
+//
+//*
+
+// <--- Поширення подій --->
+
+// const parent = document.querySelector("#parent");
+// const child = document.querySelector("#child");
+// const descendant = document.querySelector("#descendant");
+
+// parent.addEventListener("click", () => {
+//   console.log("Parent click handler");
+// });
+
+// child.addEventListener("click", () => {
+//   console.log("Child click handler");
+// });
+
+// descendant.addEventListener("click", () => {
+//   console.log("Descendant click handler");
+// });
+
+// <--- Цільовий елемент --->
+
+// const parent = document.querySelector("#parent");
+
+// parent.addEventListener("click", (event) => {
+//   console.log("event.target: ", event.target);
+//   console.log("event.currentTarget: ", event.currentTarget);
+// });
+
+// <--- Припинення спливання --->
+
+// const parent = document.querySelector("#parent");
+// const child = document.querySelector("#child");
+// const descendant = document.querySelector("#descendant");
+
+// parent.addEventListener("click", () => {
+//   alert(
+//     "Parent click handler. This alert will not appear when clicking on Descendant, the event will not reach here!"
+//   );
+// });
+
+// child.addEventListener("click", () => {
+//   alert(
+//     "Child click handler. This alert will not appear when clicking on Descendant, the event will not reach here!"
+//   );
+// });
+
+// descendant.addEventListener("click", (event) => {
+//   event.stopPropagation();
+//   alert("Descendant click handler");
+// });
+
+// <--- Делегування подій --->
+
+// const box = document.querySelector("div.box");
+
+// box.addEventListener("click", function (event) {
+//   console.log(event.target);
+// });
+
+// <--- Перевірка цільового елемента події --->
+
+const colorPalette = document.querySelector(".color-palette");
+const output = document.querySelector(".output");
+
+colorPalette.addEventListener("click", selectColor);
+
+// This is where delegation «magic» happens
+function selectColor(event) {
+  if (event.target.nodeName !== "BUTTON") {
+    return;
+  }
+
+  const selectedColor = event.target.dataset.color;
+  output.textContent = `Selected color: ${selectedColor}`;
+  output.style.color = selectedColor;
+}
+
+// Some helper functions to render palette items
+createPaletteItems();
+
+function createPaletteItems() {
+  const items = [];
+  for (let i = 0; i < 60; i++) {
+    const color = getRandomHexColor();
+    const item = document.createElement("button");
+    item.type = "button";
+    item.dataset.color = color;
+    item.style.backgroundColor = color;
+    item.classList.add("item");
+    items.push(item);
+  }
+  colorPalette.append(...items);
+}
+
+function getRandomHexColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+}
